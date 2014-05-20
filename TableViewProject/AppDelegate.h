e00da52db3243f7224494da7cb7b144942e528b2
MohamedFawzy <mfawzy22@gmail.com>
2014-05-20 02:19:42 +0200

    Another Sample Table view application

---
+++ TableViewProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewProject
+//
+//  Created by Mohamed Fawzy on 5/5/14.
+//  Copyright (c) 2014 Mohamed fawzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewProject
+//
+//  Created by Mohamed Fawzy on 5/5/14.
+//  Copyright (c) 2014 Mohamed fawzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(nonatomic,retain) IBOutlet UITableView *tableView;
+
+@end
