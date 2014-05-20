cc119099b94d30942047f2c41982fa80a3a27c58
MohamedFawzy <mfawzy22@gmail.com>
2014-05-20 02:14:49 +0200

    Sample Action Sheet Application

---
+++ ActionSheetDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ActionSheetDemo
+//
+//  Created by Mohamed Fawzy on 5/4/14.
+//  Copyright (c) 2014 Mohamed fawzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(IBAction)showSheet:(id)sender;
+
+
+@end
